6482a02a2024b46b506879c53fe86099ee0fdb19
RahmatRafiq <rahmatrafiq.1999@.com>
2025-07-17 15:58:20 +0800

    Add migration files for drivers, routes, vehicles, schedules, and bookings tables with relationships

--- database/migrations/2025_07_17_072315_create_routes_table.php
+++ database/migrations/2025_07_17_072315_create_routes_table.php
@@ -4,21 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('routes', function (Blueprint $table) {
-    $table->id();
-    $table->string('name')->nullable(); // opsional
-    $table->string('origin');
-    $table->string('destination');
-    $table->time('duration')->nullable();
-    $table->timestamps();
-});
+            $table->id();
+            $table->string('name')->nullable(); // opsional
+            $table->string('origin');
+            $table->string('destination');
+            $table->time('duration')->nullable();
+            $table->timestamps();
+        });
 
     }
 
