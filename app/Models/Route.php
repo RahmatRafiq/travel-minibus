6e121fad2b546e79b7f054916945fa5db0733477
RahmatRafiq <rahmatrafiq.1999@.com>
2025-07-17 16:08:25 +0800

    Add SoftDeletes trait to Booking, Driver, Route, Schedule, and Vehicle models

--- app/Models/Route.php
+++ app/Models/Route.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 
 class Route extends Model
 {
+        use SoftDeletes;
+
     protected $fillable = ['name', 'origin', 'destination', 'duration'];
 
     public function vehicles()
--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Vehicle extends Model
 {
+        use SoftDeletes;
+
     protected $fillable = ['plate_number', 'brand', 'seat_capacity', 'driver_id', 'route_id'];
 
     public function driver()
